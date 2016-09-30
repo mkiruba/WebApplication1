cc7529feb986c6f7702e782649484f7a1edeec1c
mkiruba <mkiruba@gmail.com>
2016-09-30 06:19:16 +0100

    Add project files.

---
+++ WebApplication1/Controllers/RentalYieldController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class RentalYieldController : ApiController
+    {
+        private const string apiKey = "psuy4qtt4u6nfy5r6mtrxdb9";
+        private const string baseurl = "http://api.zoopla.co.uk";
+        private const string apiurl = "/api/v1/property_listings.json";
+        //private const string postcode = "IG10";
+        //private const string area = "Loughton";
+
+        [Route("api/RentalYield/{postCode}/{area}")]
+        public string GetRentalYield(string postCode, string area)
+        {
+            string response;
+            using (var client = new WebClient())
+            {
+                //client.BaseAddress = new Uri(baseurl);
+                //client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var uri = $"{baseurl}{apiurl}?postcode={postCode}&area={area}&api_key={apiKey}";
+                // New code:
+                response = client.DownloadString(uri);
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    //var dataObjects = response.Content.ReadAsAsync().Result;
+                //    //foreach (var d in dataObjects)
+                //    //{
+                //    //    Console.WriteLine("{0}", d.Name);
+                //    //}
+                //}
+            }
+            return response;
+        }
+    }
+}
---
+++ WebApplication1Tests/Controllers/RentalYieldControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers.Tests
+{
+    [TestClass()]
+    public class RentalYieldControllerTests
+    {
+        [TestMethod()]
+        public void GetRentalYieldTest()
+        {
+            string postcode = "IG10";
+            string area = "Loughton";
+
+            RentalYieldController controller = new RentalYieldController();
+            controller.GetRentalYield(postcode, area);
+            Assert.Fail();
+        }
+    }
+}
